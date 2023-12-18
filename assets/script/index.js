0d4f1c945aab83e51be25c1a8c96c68967d00f2d
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-18 21:27:51 +0100

    Revert "boh"
    This reverts commit b882ccc9ea00587287e0751627398beb06df89f4.

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,19 +1,20 @@
 const buttonClick = function () {
-    window.location.href = "../HTMLs/questions.html"
+    window.location.origin = "../HTMLs/questions.html";
 };
 
-button = document.getElementById('buttonProceed')
+button = document.getElementById('buttonProceed');
 
 checkbox.addEventListener('click', function () {
     if (checkbox.checked) {
-        console.log('true')
-        button.disabled = false
-        button.classList.add('enabled')
-        button.classList.remove('disabled')
+        console.log('true');
+        button.disabled = false;
+        button.classList.add('enabled');
+        button.classList.remove('disabled');
     } else {
-        console.log('false')
-        button.disabled = true
-        button.classList.remove('enabled')
-        button.classList.add('disabled')
+        console.log('false');
+        button.disabled = true;
+        button.classList.remove('enabled');
+        button.classList.add('disabled');
     }
-})
+});
+
