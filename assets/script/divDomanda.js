91dbf841db577deb8e70ce8ab2f64c752d7b60a9
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-03-01 00:39:35 +0100

    Update questions.js e divDomanda.js

--- assets/script/divDomanda.js
+++ assets/script/divDomanda.js
@@ -1,57 +1,82 @@
-const divDomanda = function(obgDomanda){
+const divDomanda = function(fullArray, arrayRisposte, updateDinamicStage) {
+
+    const obgDomanda = fullArray[arrayRisposte.length]
     const divDomanda = document.createElement("div")
-const h5Question = document.createElement("h5")
-h5Question.innerText = obgDomanda.question
+    const h5Question = document.createElement("h5")
+    h5Question.innerText = obgDomanda.question
 
-const answerWrapper = document.createElement("div")
-answerWrapper.id ="bloccoRisposte"
+    const answerWrapper = document.createElement("div")
+    answerWrapper.id = "bloccoRisposte"
 
+    if (obgDomanda.type === "boolean") {
+        const buttonAnswer1 = document.createElement("button")
+        const buttonAnswer2 = document.createElement("button")
 
-if(obgDomanda.type === "boolean"){
-    const buttonAnswer1 = document.createElement("button")
-    const buttonAnswer2 = document.createElement("button")
+        buttonAnswer1.innerText = "Vero"
+        buttonAnswer2.innerText = "Falso"
 
-    buttonAnswer1.innerText = "Vero"
-    buttonAnswer2.innerText = "Falso"
+        // Aggiungi la classe per lo stile ai pulsanti booleani
+        buttonAnswer1.classList.add("bottoneRisposta")
+        buttonAnswer2.classList.add("bottoneRisposta")
 
-    answerWrapper.appendChild(buttonAnswer1)
-    answerWrapper.appendChild(buttonAnswer2)
+        buttonAnswer1.onclick = () => {
+            arrayRisposte.push({
+                correct_answer: obgDomanda.correct_answer,
+                userAnswer: buttonAnswer1.innerText,
+                question: obgDomanda.question,
+                incorrect_answers: obgDomanda.incorrect_answers
+            })
+            updateDinamicStage()
+        }
+        
+        buttonAnswer2.onclick = () => {
+            arrayRisposte.push({
+                correct_answer: obgDomanda.correct_answer,
+                userAnswer: buttonAnswer2.innerText,
+                question: obgDomanda.question,
+                incorrect_answers: obgDomanda.incorrect_answers
+            }) 
+            updateDinamicStage()
+        }
 
-}
-if(obgDomanda.type === "multiple"){
+        answerWrapper.appendChild(buttonAnswer1)
+        answerWrapper.appendChild(buttonAnswer2)
+    }
 
-    const buttonAnswer1 = document.createElement("button")
-    const buttonAnswer2 = document.createElement("button")
-    const buttonAnswer3 = document.createElement("button")
-    const buttonAnswer4 = document.createElement("button")
+    if (obgDomanda.type === "multiple") {
+        const answers = [
+            obgDomanda.correct_answer,
+            ...obgDomanda.incorrect_answers
+        ];
 
-    buttonAnswer1.innerText = obgDomanda.correct_answer
-    buttonAnswer2.innerText = obgDomanda.incorrect_answers[0]
-    buttonAnswer3.innerText = obgDomanda.incorrect_answers[1]
-    buttonAnswer4.innerText = obgDomanda.incorrect_answers[2]
+        const shuffleArray = (array) => {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        };
 
-    buttonAnswer1.classList.add("bottoneRisposta")
-    buttonAnswer2.classList.add("bottoneRisposta")
-    buttonAnswer3.classList.add("bottoneRisposta")
-    buttonAnswer4.classList.add("bottoneRisposta")
+        shuffleArray(answers)
 
-    const subWrapper1 = document.createElement("div")
-    const subWrapper2 = document.createElement("div")
+        answers.forEach(answer => {
+            const buttonAnswer = document.createElement("button")
+            buttonAnswer.innerText = answer
+            buttonAnswer.classList.add("bottoneRisposta")
+            buttonAnswer.onclick = () => {
+                arrayRisposte.push({
+                    correct_answer: obgDomanda.correct_answer,
+                    userAnswer: buttonAnswer.innerText,
+                    question: obgDomanda.question,
+                    incorrect_answers: obgDomanda.incorrect_answers
+                })
+                    updateDinamicStage()
+            }
+            answerWrapper.appendChild(buttonAnswer)
+        })
+    }
 
-    subWrapper1.classList.add("rigaRisposte1")
-    subWrapper2.classList.add("rigaRisposte2")
-
-    subWrapper1.appendChild(buttonAnswer1)
-    subWrapper1.appendChild(buttonAnswer2)
-    subWrapper2.appendChild(buttonAnswer3)
-    subWrapper2.appendChild(buttonAnswer4)
-    answerWrapper.appendChild(subWrapper1)
-    answerWrapper.appendChild(subWrapper2)
-}
-
-
-divDomanda.appendChild(h5Question)
-divDomanda.appendChild(answerWrapper)
+    divDomanda.appendChild(h5Question)
+    divDomanda.appendChild(answerWrapper)
     return divDomanda
 }
 
--- assets/script/questions.js
+++ assets/script/questions.js
@@ -4,31 +4,48 @@ const apiUrl = "https://opentdb.com/api.php?amount=50&category=15"
 const body = document.getElementsByName("body")[0]
 const dinamicStage = document.getElementById("dinamicStage")
 const arrayLeaderBoard = []
-const arrayRisposte = []
 let arrayDomande = []
+const arrayRisposte = []
 
 
 let nDomandeFatte = 0
 let username
 let intervalloUnico
 let currentQuestion = {}
+//////////////////////////////////////////////// UTILITY ////////////////////////////////////////
+
+const updateDinamicStage = () => {
+    while (dinamicStage.firstChild) {
+        dinamicStage.removeChild(dinamicStage.firstChild)
+    }
+    if(arrayDomande.length === arrayRisposte.length){
+        location.href = "leaderboard.html"
+    } else {
+        dinamicStage.appendChild(divDomanda(arrayDomande, arrayRisposte, updateDinamicStage))
+    }
+}
+
 //////////////////////////////////////////////////////// Metodo FETCH /////////////////////////////////////////////////////////
 const decodificaCaratteriHTML = (arrayQuestions) => {
-
     const htmlEntities = {
         '&quot;': '"',
         '&amp;': '&',
         '&lt;': '<',
         '&gt;': '>',
-        '&#039;': "'"
+        '&#039;': "'",
+        '&Acute;': '´', // Aggiunta dell'entità per l'accento acuto
         // Aggiungi altre entità qui se necessario
-      }
+    };
+    
+    for (let index = 0; index < arrayQuestions.length; index++) {
+        arrayQuestions[index].question = arrayQuestions[index].question.replace(/&quot;|&amp;|&lt;|&gt;|&#039;|&Acute;/g, (entity) => htmlEntities[entity]);
+        arrayQuestions[index].correct_answer = arrayQuestions[index].correct_answer.replace(/&quot;|&amp;|&lt;|&gt;|&#039;|&Acute;/g, (entity) => htmlEntities[entity]);
+        for (let bindex = 0; bindex < arrayQuestions[index].incorrect_answers.length; bindex++) {
+            arrayQuestions[index].incorrect_answers[bindex] = arrayQuestions[index].incorrect_answers[bindex].replace(/&quot;|&amp;|&lt;|&gt;|&#039;|&Acute;/g, (entity) => htmlEntities[entity]);
+        }
+    }
+};
 
-for (let index = 0; index < arrayQuestions.length; index++) {
-    arrayQuestions[index].question = arrayQuestions[index].question.replace(/&quot;|&amp;|&lt;|&gt;/g, (entity) => htmlEntities[entity]);
-}
-
-}
 
 const diffInSecondi = function (diffString) {
     switch (diffString) {
@@ -60,7 +77,6 @@ const fetchQuestions = async () => {
                 }
             })
         }
-        console.log(newArray)
         start(newArray)
     } catch (error) {
         console.log(error)
@@ -72,7 +88,7 @@ const start = (arrayQuestions) => {
 decodificaCaratteriHTML(arrayQuestions)
 
 dinamicStage.innerHTML = ``
-dinamicStage.appendChild(divDomanda(arrayQuestions[0]))
+dinamicStage.appendChild(divDomanda(arrayQuestions, arrayRisposte, updateDinamicStage))
 
 }
 
