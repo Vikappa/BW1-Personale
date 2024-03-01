db4772e8c9d40d8ed2e2546c88753160d43b9293
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-03-01 21:23:14 +0100

    Update index.js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,30 +1,72 @@
-const button = document.getElementById('buttonProceed');
-const checkbox = document.getElementById('checkbox');
+const button = document.getElementById('buttonProceed')
+const checkbox = document.getElementById('checkbox')
+const inputName = document.getElementById('inputNome')
+const fileInput = document.getElementById('imageInput')
+const saveButton = document.getElementById('saveButton')
 
-
-button.addEventListener('click', buttonClick);
+button.addEventListener('click', buttonClick)
 
 function buttonClick() { 
-    if (checkbox.checked) {
+    if (checkbox.checked && inputName.value.length) {
+        sessionStorage.setItem("nomePartecipante", JSON.stringify(inputNome.value))
         window.location.href = "./HTMLs/questions.html"
     }
 }
 
-
-checkbox.addEventListener('click', function () {
-
-    if (checkbox.checked) {
-        console.log('true');
-        button.disabled = false;
-        button.classList.add('enabled');
-        button.classList.remove('disabled');
+function updateButtonState() {
+    if (checkbox.checked && inputName.value.length) {
+        button.disabled = false
+        button.classList.add('enabled')
+        button.classList.remove('disabled')
     } else {
-        console.log('false');
-        button.disabled = true;
-        button.classList.remove('enabled');
-        button.classList.add('disabled');
+        button.disabled = true
+        button.classList.remove('enabled')
+        button.classList.add('disabled')
     }
 }
-)
+
+checkbox.addEventListener('click', updateButtonState)
+inputName.addEventListener('input', updateButtonState)
+
+document.getElementById('imageInput').addEventListener('change', function() {
+
+    if (fileInput.files && fileInput.files[0]) {
+        saveButton.style.display = 'block';
+    } else {
+        saveButton.style.display = 'none';
+    }
+})
 
 
+
+function saveImageToSessionStorage() {
+    if (fileInput.files && fileInput.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            var img = new Image();
+            
+            img.onload = function() {
+                var canvas = document.createElement('canvas')
+                var ctx = canvas.getContext('2d')
+                canvas.width = 120
+                canvas.height = 120
+                ctx.drawImage(img, 0, 0, 120, 120)
+                
+                var resizedImage = canvas.toDataURL('image/png')
+                sessionStorage.setItem('profilePic', resizedImage)
+                alert('Immagine ridimensionata e salvata in sessionStorage!')
+            }
+            
+            img.src = e.target.result
+        }
+        
+        reader.readAsDataURL(fileInput.files[0])
+    } else {
+        alert('Per favore, seleziona un file prima di provare a salvarlo.')
+    }
+}
+
+saveButton.addEventListener("click", function () {
+    saveImageToSessionStorage()
+})
